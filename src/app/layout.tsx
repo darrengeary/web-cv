6080543f02732f48fba485d0cca987350b6a42f2
darrengeary <gearydarren@gmail.com>
2026-01-05 16:55:40 +0000

    Final dark mode fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html className="dark" lang="en" suppressHydrationWarning>
       <body>
         <main className="min-h-[calc(100vh-140px)]">{children}</main>
         <Footer />
